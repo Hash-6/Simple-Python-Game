0f788a8b01274b969172f89fe370e73029e90ab9
Hash6 <118583054+Hash-6@users.noreply.github.com>
2023-02-18 17:16:04 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,32 +1,30 @@
-#MADE BY HASH6 (DO NOT REMOVE THIS)
-text = input("You find yourself in a dark hallway. Would you like to go left or right? ")
-if text.lower() == "right": 
-    print("You die.")
-elif text.lower() == "left": 
-    second_text = input("You fall down a trapdoor into another room. There are 2 doors. Do you go into the front or back door?")
-    if second_text.lower() == "back":
-        third_text = input("The door leads into a room with 3 potions. There is one in the left corner on the room, the right corner of the room, and the middle. Which one do you drink?")
-    elif second_text.lower() == "front":
-        print("You die.")
-    else:
-        print("say front or back as your answer")
-
-if third_text.lower() == "middle":
-  third_text = input("You fall asleep and awake in a room with a pig portal. Do you go in?")
-elif third_text.lower() == "left":
-  text = third_text = input("You fall asleep and awake in a cave underground.")
-elif third_text.lower() == "right":
-  print("you die")
-else:
-  print("say middle, left, or right as your answer")
-
-if third_text.lower() == "yes":
-  fourth_text = input("You go into the portal and find yourself in some type of colorful jungle. There is a path. Do you go left, right, or forward?")
-  if fourth_text.lower() == "left":
-    print("you die")
-  elif fourth_text.lower() == "right":
-    fifth_text = "?"
-  elif fourth_text.lower() == "forward":
-    print("you die")
-  else:
-    print("type forward or Forward as your answer.")
+text = input("You find yourself in a dark hallway. Would you like to go left or right?").lower()
+match text:
+  case "left":
+    second_text = input("You see a trapdoor and under it is a ladder that you climb down into a room with 2 doors. Would you like to go into the back or front door?").lower()
+    match second_text:
+      case "back":
+        third_text = input("You climb down a ladder into a room with three potions. There is one in the right and left corner, and one in the middle. Which one do you drink?").lower()
+#2
+        match third_text:
+          case "middle":
+            fourth_text = input("You fall asleep and awake in a room with a pig portal. Do you go in?").lower()
+            match fourth_text:
+              case "yes":
+                sixth_text = input("You go into the portal and find yourself in some type of colorful jungle. There is a path. Do you go left, right, or forward?").lower()
+              case "no":
+                print("Say yes")
+          case "left":
+            fith_text = input("You fall asleep and awake in a cave underground. There are 2 levers on each side of you. Which one do you pull?").lower()
+#3
+            match fith_text:
+              case "right":
+                seventh_text = input("You go into the portal and find yourself in some type of colorful jungle. There is a path. Do you go left, right, or forward?").lower()
+              case "left", "right":
+                print("You die")
+          case "right":
+            print("You die")
+      case "front":
+        print("You die")
+  case "right":
+    print("You die")
